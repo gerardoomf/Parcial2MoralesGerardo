e94a872dcf2de1a5267d72034bd27781fa1fed4e
Gera <gerafranmora@gmail.com>
2024-10-23 13:31:00 -0600

    Add files via upload

--- Examen.php
+++ Examen.php
@@ -7,30 +7,38 @@
 </head>
 <body>
 
-<?php
-$Telefonos = array('Iphone' => 25000,'Samsung' => 20000,'Huawei' => 15000,'Oppo' => 10000,'Vivo' => 10000);
+<?php 
+
+$Telefonos =[["Telefono" => "Iphone", "Precio" => 25000],["Telefono" => "Samsung", "Precio" => 23000]
+,["Telefono" => "Motorola", "Precio" => 12000],["Telefono" => "Huawei", "Precio" => 1500],
+["Telefono" => "Oppo", "Precio" => 20000]];
+
+echo "Telefonos: ";
+echo "<br>"; 
+
+	foreach ($Telefonos as $Telefono) {
+    	echo $Telefono["Telefono"] . " Precio: " . $Telefono["Precio"];
+    	echo "<br>";
+	}
+
+array_push($Telefonos, ["Telefono" => "Vivo", "Precio" => 5000]);
+array_push($Telefonos, ["Telefono" => "Nokia", "Precio" => 22000]);
+echo "<br>";
+
+echo "Telefonos nuevos: "; 
+echo "<br>"; 
+
+	foreach ($Telefonos as $Telefono) {
+    	echo $Telefono["Telefono"] . " Precio: " . $Telefono["Precio"];
+    	echo "<br>";
+	}
+
+usort($Telefonos, function($a, $b) {
+    	return $a["Precio"] <=> $b["Precio"];
+	});
 
 
-$indexado = array();
-foreach ($Telefonos as $marca => $precio) {
-    $indexado[] = array('Marca' => $marca, 'Precio' => $precio);
-}
-
-
-
-
-
-$Carros = array();
-foreach ($indexado as $item) {
-    $Telefonos[$item['Marca']] = $item['Precio'];
-}
-
-
-print_r($Telefonos);
-asort($Telefonos);
-print_r($Telefonos);
-?>
-
+ ?>
 
 </body>
 </html>
